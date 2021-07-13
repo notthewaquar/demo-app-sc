b013b9a176d5ecaed239d841e4d8375d2525f574
abdul waquar <notthewaquar@gmail.com>
2021-07-13 16:16:25 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'srvcrm';
+
+  // SIDE BAR CODE
+  sidebarOpen = false;
+  dropdown1 = false;
+  dropdown2 = false;
+  dropdown3 = false;
+ 
+}
